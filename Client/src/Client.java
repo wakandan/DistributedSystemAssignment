9b8ca8b01e382aa54169a1a5b271778bf8b4aaf1
Dang Nguyen Anh Khoa <wakandan@gmail.com>
2012-02-24 16:57:52 +0800

    - Added client class and generic byte stream sending routine to send data.   It was the base to add a function to send string - Add an inteface contains all the constants that will be used in the   project

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,30 +1,59 @@
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
+public class Client implements Constants {
+	public String			serverIp;
+	public int				serverPort;
+	public byte[]			buffer;
+	public DatagramSocket	socket;
+	public InetAddress		server;
 
-public class Client {
-	public static void main(String args[]){
-		DatagramSocket aSocket =null;
-		try{
-			aSocket = new DatagramSocket();
-			String text ="test";
-			byte[] m = text.getBytes();
-			InetAddress aHost = Inet4Address.getByName("192.168.1.14");
-			int serverPort = 6789;
-			DatagramPacket request = new DatagramPacket(m,m.length, aHost, serverPort);
-			aSocket.send(request);
-			byte [] buffer =new byte[1000];
-			DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
-			aSocket.receive(reply);
-			System.out.println("rely : "+new String(reply.getData()));
-		}catch(Exception e){
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
-		finally{ 
-			if(aSocket!=null)aSocket.close();
-		}
+	public Client(String serverIp, int serverPort) throws SocketException, UnknownHostException {
+		this.serverIp = serverIp;
+		this.serverPort = serverPort;
+		this.buffer = new byte[BUFFER_SIZE];
+		this.socket = new DatagramSocket();
+		this.server = InetAddress.getByName(this.serverIp);
+	}
+
+	public void send(byte[] bytes, int length) throws IOException {
+		DatagramPacket request = new DatagramPacket(bytes, length, this.server, this.serverPort);
+		this.socket.send(request);
+	}
+
+	public void send(String str) throws IOException {
+		send(str.getBytes(), str.length());
+	}
+
+	public void recv(byte[] bytes, int maxLen) throws IOException {
+		DatagramPacket request = new DatagramPacket(bytes, maxLen);
+		this.socket.receive(request);
+	}
+
+	/* A typical command wherein the request is constructed using pre-defined
+	 * keys and supplied values. The given strings will then be send to server */
+	public String readFile(String filename, int byteOffset, int length) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(KEY_CMD + ":" + VAL_CMD_READFILE);
+		sb.append(KEY_FILENAME + ":" + filename + "\n");
+		sb.append(KEY_OFFSET + ":" + byteOffset + "\n");
+		sb.append(KEY_LENGTH + ":" + length + "\n");
+		sb.append("\n\n");
+		return sb.toString();
+	}
+
+	public static void main(String args[]) throws IOException {
+		String serverIp = "127.0.0.1";
+		int serverPort = 6789;
+		Client client = new Client(serverIp, serverPort);
+		client.send("test");
+		byte[] reply = new byte[1000];
+		client.recv(reply, reply.length);
+		System.out.println(new String(reply));
 	}
 }
---
+++ Client/src/Constants.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+
+/**
+ * @author akai
+ * 
+ */
+public interface Constants {
+	public static final int		BUFFER_SIZE			= 10000;
+	public static final String	KEY_FILENAME		= "Filename";
+	public static final String	KEY_OFFSET			= "Offset";
+	public static final String	KEY_LENGTH			= "Length";
+	public static final String	KEY_CMD				= "Command";
+	public static final String	VAL_CMD_READFILE	= "Readfile";
+}
